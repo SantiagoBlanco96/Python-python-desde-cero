67c144d8597892c1f40c1458f0efe3f6c3299bd5
SantiagoBlanco96 <63128092+SantiagoBlanco96@users.noreply.github.com>
2024-02-14 18:35:00 +0000

    Empezando a aprender python

--- hola_mundo.py
+++ hola_mundo.py
@@ -4,6 +4,7 @@
 print("Hola Python")
 print('Hola Python')
 
+
 """  
     Esto es un 
     comentario 
@@ -16,3 +17,10 @@ print('Hola Python')
     en varias lineas
 '''
 
+# Consultar el tipo de dato
+
+print(type("Hola Python")) # Tipo 'str'
+print(type(10)) # Tipo 'int'
+print(type(10.5)) # Tipo 'float'
+print(type(True)) # Tipo 'bool'
+print(type(1 + j)) # Tipo 'complex'
