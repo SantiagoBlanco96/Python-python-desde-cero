a811d7251616e7f5b27b25efc65c4e97ec19fb48
SantiagoBlanco96 <63128092+SantiagoBlanco96@users.noreply.github.com>
2024-02-14 18:30:39 +0000

    Empezando a aprender python

---
+++ hola_mundo.py
@@ -0,0 +1,18 @@
+# Esto es un comentario
+# Hola mundo 
+
+print("Hola Python")
+print('Hola Python')
+
+"""  
+    Esto es un 
+    comentario 
+    en varias lineas
+"""
+
+'''
+    Este tambíen
+    es un comentario
+    en varias lineas
+'''
+
