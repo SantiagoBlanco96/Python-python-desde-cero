48df7f376669624eca293eb39c95c08dc78b4cbd
Santiago <sam96.sb73@gmail.com>
2024-02-15 08:40:37 -0300

    Termina la clase de variables

--- 01_variables.py
+++ 01_variables.py
@@ -1,5 +1,4 @@
 # Variables
-
 my_string_variable = "My String variable"
 print(my_string_variable)
 
@@ -13,4 +12,23 @@ print(type(my_int_to_str_variable))
 my_bool_variable = True
 print(my_bool_variable)
 
+# Concatenación de variables en un print
 print(my_string_variable, my_int_variable, my_bool_variable)
+print("Este es el valor de la variable: ", my_bool_variable)
+
+# Funciones del sistema
+print(len(my_string_variable))
+
+# Variables en una sola línea, se puede hacer pero no es una buena práctica!!!
+name, surname, alias, age = "Juan", "Perez", "JP", 25
+print("me llamo:", name, surname, ". Mi edad es", age, ". Y mi alias es", alias)
+
+# La forma correcta de definir variables en python es
+name = "Juan"
+surname = "Perez"
+alias = "JP"
+age = 25
+
+# Inputs
+name = input("Ingrese su nombre: ")
+print("Hola", name)
