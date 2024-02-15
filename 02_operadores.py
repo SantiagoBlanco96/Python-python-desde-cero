d4ca35046186b1c0ebe058480e95364a27ba3912
Santiago <sam96.sb73@gmail.com>
2024-02-15 09:37:20 -0300

    Clase de operadores

---
+++ 02_operadores.py
@@ -0,0 +1,28 @@
+# Operadores
+print(3 + 4) # Suma 
+print(3 - 4) # Resta
+print(3 * 4) # Multiplicación
+print(3 ** 4) # Potencia
+print(3 / 4) # División
+print(10 % 4) # Módulo (resto de la división)
+print(3 // 4) # División entera (sin decimales)
+
+print("Hola " + "Python") # Concatenación de strings
+print("Hola " * 3) # Multiplicación de strings
+
+# Operadores de comparación
+print(3 == 4) # Igualdad
+print(3 != 4) # Desigualdad
+print(3 > 4) # Mayor que
+print(3 < 4) # Menor que
+print(3 >= 4) # Mayor o igual que
+print(3 <= 4) # Menor o igual que
+
+print("Hola" > "Python") # Comparación de strings (orden alfabético)
+print("Hola" == "Hola") # Comparación de strings (igualdad)
+print("Hola" != "Python") # Comparación de strings (desigualdad)
+
+# Operadores lógicos
+print(True and True) # AND
+print(3 > 4 or "Hola" > "Python") # OR
+print(not(3 > 4 or "Hola" > "Python")) # NOT
