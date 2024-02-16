e026c8063214b141cc9f37814c2026a5f4bd76cd
Santiago <sam96.sb73@gmail.com>
2024-02-16 11:39:07 -0300

    Clase condicionales

---
+++ 08_condicionales.py
@@ -0,0 +1,22 @@
+# Condicionales 
+
+my_condition = True
+
+if my_condition:
+    print("La condición es verdadera")
+
+print("La ejecucción continúa...")
+
+my_condition = 5 * 2
+
+if my_condition > 10:
+    print("Es mayor a 10")
+else:
+    print("Es menor o igual a 10")
+
+if my_condition > 10:
+    print("Es mayor a 10")
+elif my_condition == 10:
+        print("Es igual a 10")
+else:
+    print("Es menor a 10")
