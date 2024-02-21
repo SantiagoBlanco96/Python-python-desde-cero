b424135b6c66e2358d6a373cc5c9f5ce6245cb53
Santiago <sam96.sb73@gmail.com>
2024-02-21 16:09:01 -0300

    Seguimos practicando

--- 99_homework.py
+++ 99_homework.py
@@ -111,3 +111,197 @@
 # del beatles[-1]
 # print("Paso 4: ", beatles)
 # beatles.insert(0, "Ringo Starr")
+# my_list = [[0, 1, 2, 3] for i in range(2)]
+# print(my_list[2][0])
+
+"""
+4.3.4   LAB   Un año bisiesto: escribiendo tus propias funciones
+Tu tarea es escribir y probar una función que toma un argumento (un año) y devuelve True si el año es un año bisiesto, o False si no lo es.
+
+La semilla de la función ya se muestra en el código esqueleto del editor.
+
+Nota: también hemos preparado un breve código de prueba, que puedes utilizar para probar tu función.
+
+El código utiliza dos listas - una con los datos de prueba y la otra con los resultados esperados. El código te dirá si alguno de tus resultados no es válido.
+"""
+# Mi solucion
+
+# def is_year_leap(year):
+#     if (year%4 == 0) and (not(year%100 == 0) or year%400 == 0):
+#         return True
+#     else:
+#         return False
+
+# test_data = [1900, 2000, 2016, 1987]
+# test_results = [False, True, True, False]
+# for i in range(len(test_data)):
+#     yr = test_data[i]
+#     print(yr,"->",end="")
+#     result = is_year_leap(yr)
+#     if result == test_results[i]:
+#         print("OK")
+#     else:
+#         print("Fallido")
+
+"""
+4.3.5   LAB   Cuántos días: escribiendo y usando tus propias funciones
+Tu tarea es escribir y probar una función que toma dos argumentos (un año y un mes) y devuelve el número de días del mes/año dado (mientras que solo febrero es sensible al valor year, 
+tu función debería ser universal).
+
+La parte inicial de la función está lista. Ahora, haz que la función devuelva None si los argumentos no tienen sentido.
+
+Por supuesto, puedes (y debes) utilizar la función previamente escrita y probada (LAB 4.3.1.6). Puede ser muy útil. 
+Te recomendamos que utilices una lista con los meses. Puedes crearla dentro de la función - este truco acortará significativamente el código.
+
+Hemos preparado un código de prueba. Amplíalo para incluir más casos de prueba.
+
+"""
+
+# Mi solucion
+
+# def is_year_leap(year):
+#     if (year%4 == 0) and (not(year%100 == 0) or year%400 == 0):
+#         return True
+#     else:
+#         return False
+
+# def days_in_month(year, month):
+#     month_30 = [11,4,7,9]
+#     if year < 1582 or month < 1 or month > 12:
+#        return None
+#     if month in month_30:
+#         return 30
+#     elif month != 2:
+#         return 31
+#     else:
+#         if is_year_leap(year):
+#             return 29
+#         else:
+#             return 28
+
+# test_years = [1900, 2000, 2016, 1987]
+# test_months = [2, 2, 1, 11]
+# test_results = [28, 29, 31, 30]
+# for i in range(len(test_years)):
+#     yr = test_years[i]
+#     mo = test_months[i]
+#     print(yr, mo, "->", end="")
+#     result = days_in_month(yr, mo)
+#     if result == test_results[i]:
+#         print("OK")
+#     else:
+#         print("Fallido")
+
+"""
+4.3.6   LAB   Día del año: escribiendo y usando tus propias funciones
+Tu tarea es escribir y probar una función que toma tres argumentos (un año, un mes y un día del mes) 
+y devuelve el día correspondiente del año, o devuelve None si cualquiera de los argumentos no es válido.
+
+Debes utilizar las funciones previamente escritas y probadas. Agrega algunos casos de prueba al código. 
+Esta prueba es solo el comienzo.
+"""
+# def is_year_leap(year):
+#     if (year%4 == 0) and (not(year%100 == 0) or year%400 == 0):
+#         return True
+#     else:
+#         return False
+
+# def days_in_month(year, month):
+    
+#     month_30 = [11,4,7,9]
+    
+#     if year < 1582 or month < 1 or month > 12:
+#        return None
+       
+#     if month in month_30:
+#         return 30
+#     elif month != 2:
+#         return 31
+#     else:
+#         if is_year_leap(year):
+#             return 29
+#         else:
+#             return 28
+
+# def day_of_year(year, month, day):
+    
+#     total = 0
+    
+#     if day > days_in_month(year, month):
+#         return None
+#     for m in range(1,month):
+#         total += days_in_month(year, m)
+    
+#     total += day	
+#     return total
+
+# print(day_of_year(2000, 2, 29))
+"""
+4.3.7   LAB   Números primos - cómo encontrarlos
+Un número natural es primo si es mayor que 1 y no tiene divisores más que 1 y si mismo.
+
+¿Complicado? De ningúna manera. Por ejemplo, 8 no es un número primo, ya que puedes dividirlo entre 2 y 4 (no podemos usar divisores iguales a 1 y 8, ya que la definición lo prohíbe).
+
+Por otra parte, 7 es un número primo, ya que no podemos encontrar ningún divisor para el.
+
+Tu tarea es escribir una función que verifique si un número es primo o no.
+
+La función:
+
+se llama is_prime;
+toma un argumento (el valor a verificar)
+devuelve True si el argumento es un número primo, y False de lo contrario.
+Sugerencia: intenta dividir el argumento por todos los valores posteriores (comenzando desde 2) y verifica el resto - si es cero, tu número no puede ser un número primo; analiza cuidadosamente cuándo deberías detener el proceso.
+
+Si necesitas conocer la raíz cuadrada de cualquier valor, puedes utilizar el operador **. Recuerda: la raíz cuadrada de x es lo mismo que x0.5.
+
+Complementa el código en el editor.
+
+"""
+# # Solucion 1
+# def is_prime(num):
+    
+#     num_is_prime = True
+    
+#     for i in range(2,num):
+#         if num % i == 0:
+#             num_is_prime = False
+#     return num_is_prime
+
+# for i in range(1, 20):
+#     if is_prime(i + 1):
+#         print(i + 1, end=" ")
+# print()
+# # Solucion 2
+# def is_prime(num):
+#     for i in range(2, int(1 + num ** 0.5)):
+#         if num % i == 0:
+#             return False
+#     return True
+
+# for i in range(1, 20):
+#     if is_prime(i + 1):
+#         print(i + 1, end=" ")
+# print()
+
+my_list =  ['Mary', 'had', 'a', 'little', 'lamb']
+
+
+def my_list(my_list):
+    del my_list[3]
+    my_list[3] = 'ram'
+
+
+print(my_list(my_list))
+
+
+
+
+
+
+
+
+
+
+
+ 
