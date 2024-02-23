44f5f27e937673230e092f0ddb1220a836d1c612
Santiago <sam96.sb73@gmail.com>
2024-02-23 14:01:11 -0300

    Primera version tic tac toe 1.0

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,4 +1,8 @@
-def show_board(board):
+from random import randrange
+
+# Funciones
+
+def show_board(board): # Esta funcion muestra el tablero del juego en el estado actual
     print(f"""
     +-------+-------+-------+
     |       |       |       |
@@ -15,7 +19,7 @@ def show_board(board):
     +-------+-------+-------+
     """)
 
-def star_game():
+def star_game(): # Setea una lista que representa el tablero y una lista que guarda los movimientos disponibles
     board = []
     boxes = []
     case = 1
@@ -28,7 +32,7 @@ def star_game():
         board.append(row) 
     return board, boxes
 
-def add_play(board, player, position):
+def add_play(board:list, player:str, position:int): # Modifica el tablero agregando la jugada del jugador
     for i in range(3):
         for j in range(3):
             if board[i][j] == position:
@@ -36,41 +40,90 @@ def add_play(board, player, position):
                 return board
     return board
 
-def end_game(board):
+def end_game(board:list): #Verifica que haya un ganador
     if board[0][0] == board[0][1] == board [0][2]:
-        return True
+        return True, board[0][0]
     elif board[1][0] == board[1][1] == board [1][2]:
-        return True
+        return True, board[1][0]
     elif board[2][0] == board[2][1] == board [2][2]:
-        return True
+        return True, board[2][0]
     elif board[0][0] == board[1][0] == board [2][0]:
-        return True
+        return True, board[0][0]
     elif board[0][1] == board[1][1] == board [2][1]:
-        return True
+        return True, board[0][1]
     elif board[0][2] == board[1][2] == board [2][2]:
-        return True
+        return True, board[0][2]
     elif board[0][0] == board[1][1] == board [2][2]:
-        return True
+        return True, board[0][0]
     elif board[0][2] == board[0][1] == board [2][0]:
-        return True
+        return True, board[0][2]
     else:
-        return False
+        return False, " "
+
+def move_machine(avaliable_moves:list, game_board:list): # Realiza un movimiento aleatorio, es el juego de la maquina
+    move = 0
+    if len(avaliable_moves)!= 0:
+        if 5 in avaliable_moves:
+            move = 5
+        else:
+            while move not in avaliable_moves:
+                move = randrange(1,9)            
+        avaliable_moves.remove(move)
+        game_board = add_play(game_board,player_machine,move)    
+    return avaliable_moves, game_board
+
+def move_user(avaliable_moves:list, game_board:list): #Le pide al usuario que ingrese un movimiento y lo aplica
+    move = 0
+    while move == 0:
+        
+        try:
+            move = int(input("Ingresa tu movimiento: "))
+            if not (move > 0 and move < 10):
+                print("Parece que ingresaste un número que no se encuentra en el tablero, vuelve a intentarlo")
+                move = 0
+            elif move not in avaliable_moves:
+                print("El movimiento que ingresaste no se encuentra disponible, vuelve a intentarlo")
+                move = 0
+        except ValueError:
+            print("Solo puedes ingresar números, vuelve a intentarlo")
+            move = 0
+        except:
+            print("Parece que algo salió mal, vuelve a intentarlo")
+            move = 0
+    
+    avaliable_moves.remove(move)
+    game_board = add_play(game_board,player_user,move)
+        
+    return avaliable_moves, game_board
+
+# Juego
 
 game_board, avaliable_moves = star_game()
-show_board(game_board)
-print(avaliable_moves)
-game_board = add_play(game_board,"X",1)
-avaliable_moves.remove(1)
-show_board(game_board)
-print(avaliable_moves)
-game_board = add_play(game_board,"X",2)
-avaliable_moves.remove(2)
-show_board(game_board)
-print(avaliable_moves)
-game_board = add_play(game_board,"X",3)
-avaliable_moves.remove(3)
-show_board(game_board)
-print(avaliable_moves)
-print(end_game(game_board))
+player_user = "O"
+player_machine = "X"
 
+print("Vamos a jugar un TIC TAC TOE!")
+show_board(game_board)
 
+print("Comienzo yo!")
+avaliable_moves, game_board = move_machine(avaliable_moves,game_board)
+end, ganador = end_game(game_board)
+
+while not end and len(avaliable_moves) != 0 :
+    show_board(game_board)
+    avaliable_moves, game_board = move_user(avaliable_moves,game_board)
+    end, ganador = end_game(game_board)
+    if end:
+        break
+    print("Es mi turno")
+    avaliable_moves, game_board = move_machine(avaliable_moves,game_board)
+    end, ganador = end_game(game_board)
+
+show_board(game_board)
+end, ganador = end_game(game_board)
+if not end and len(avaliable_moves) == 0:
+    print("Empatamos!")
+elif ganador == "X": 
+    print("Perdiste!")
+else:
+    print("Has Ganado!")
